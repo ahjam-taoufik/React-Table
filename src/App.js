69c64fabc7f1686a39f21d42b586aafc57573248
taoufik ahjam <taoufikahjam@gmail.com>
2022-01-10 21:37:22 +0100

    basic table

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BasicTable } from "./components/BasicTable";
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+       <BasicTable/>
+    
+    </div>
+  );
+}
+
+export default App;
